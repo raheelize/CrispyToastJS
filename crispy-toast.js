3de1cf34af0d8bd5437554c7afca71e83529d28c
Raheel Asghar <raheel.ghauri@nayatel.com>
2023-08-22 10:33:25 +0500

    2.0

--- crispy-toast.js
+++ crispy-toast.js
@@ -8,7 +8,7 @@ CrispyToast.createToast = function(message, options) {
   options = options || {};
   var toast = {
     message: message,
-    type: options.type || 'info',
+    type: options.type || 'message',
     position: options.position || 'top-right',
     timeout: options.timeout || 3000
   };
@@ -16,19 +16,26 @@ CrispyToast.createToast = function(message, options) {
   CrispyToast.toasts.push(toast);
 
   CrispyToast.renderToast(toast);
+};
+CrispyToast.renderToast = function(toast) {
+  var toastContainer = document.createElement('div');
+  toastContainer.className = 'crispy-toast ' + toast.type + ' ' + toast.position;
+
+  // Create an element for the message
+  var messageElement = document.createElement('span');
+  messageElement.className = 'toast-message';
+  messageElement.textContent = toast.message;
+
+  toastContainer.appendChild(messageElement);
+
+  document.body.appendChild(toastContainer);
 
   setTimeout(function() {
     CrispyToast.removeToast(toast);
   }, toast.timeout);
 };
 
-CrispyToast.renderToast = function(toast) {
-  var toastContainer = document.createElement('div');
-  toastContainer.className = 'crispy-toast ' + toast.type + ' ' + toast.position;
-  toastContainer.textContent = toast.message;
 
-  document.body.appendChild(toastContainer);
-};
 
 CrispyToast.removeToast = function(toast) {
   var index = CrispyToast.toasts.indexOf(toast);
@@ -36,10 +43,16 @@ CrispyToast.removeToast = function(toast) {
     CrispyToast.toasts.splice(index, 1);
   }
 
-  var toastElement = document.querySelector('.crispy-toast');
-  if (toastElement) {
-    document.body.removeChild(toastElement);
+  var toastElements = document.querySelectorAll('.crispy-toast.' + toast.position);
+  if (toastElements.length > 0) {
+    toastElements.forEach(function(element) {
+      document.body.removeChild(element);
+    });
   }
+};
+
+CrispyToast.message = function(message, options) {
+  CrispyToast.createToast(message, Object.assign({}, options, { type: 'message' }));
 };
 
 CrispyToast.success = function(message, options) {
